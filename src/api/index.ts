cb8de2808c926ea9330997ac58563167a236562c
M4litt <M4litt>
2023-04-03 17:44:33 -0300

    swagger de mierda

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,19 @@
 import express from "express"
 import mongoose from "mongoose"
+
+import swaggerUiEexpress from 'swagger-ui-express'
 import { dolar_router } from "./routes/dolar"
+import { swaggerDocs } from "./routes/api_docs"
+
 require("dotenv").config()
 
 const app: express.Application = express()
 const port: number = 38406
 
-app.use(express.json())
-app.use('/', dolar_router)
+app
+.use(express.json())
+    .use('/api_docs', swaggerUiEexpress.serve, swaggerUiEexpress.setup(swaggerDocs))
+    .use('/', dolar_router)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
--- src/api/routes/dolar.ts
+++ src/api/routes/dolar.ts
@@ -1,16 +1,27 @@
+/**
+*   @swagger
+*   components:
+*       schemas:
+*           dolar:
+*               type: object
+*               properties: 
+*                   id: 
+*                       type: string
+*                       description: leto
+*/
+
 import express from "express"
 import dolar_controller from "../controllers/dollar_controller"
 
 export const dolar_router = express.Router()
 
-dolar_router
-    .get('/', dolar_controller.get_all_dollars)
-    .post('/', dolar_controller.post_dollar)
-    .patch('/', dolar_controller.patch_dollar)
-    .delete('/', dolar_controller.delete_dollar)
-    .put('/', dolar_controller.put_dollar)
+dolar_router.get('/', dolar_controller.get_all_dollars)
+dolar_router.post('/', dolar_controller.post_dollar)
+dolar_router.patch('/', dolar_controller.patch_dollar)
+dolar_router.delete('/', dolar_controller.delete_dollar)
+dolar_router.put('/', dolar_controller.put_dollar)
 
-    .get('/promedio_dolares', dolar_controller.promedio_dolares)
-    .get('/:dolar_name', dolar_controller.get_dollar)
-    .patch('/:dolar_name/save_values', dolar_controller.save_values)
+dolar_router.get('/promedio_dolares', dolar_controller.promedio_dolares)
+dolar_router.get('/:tipo', dolar_controller.get_dollar)
+dolar_router.patch('/:tipo/save_values', dolar_controller.save_values)
     
